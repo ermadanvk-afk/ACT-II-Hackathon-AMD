bae55e4a50c000381b4b1947e377a59996555af9
ermadanvk-afk <er.madanvk@gmail.com>
2026-07-10 21:25:31 +0530

    env path fix

--- agent1/config/llm_config.py
+++ agent1/config/llm_config.py
@@ -1,9 +1,8 @@
 import os
 from dotenv import load_dotenv
 from crewai import LLM
-
-load_dotenv()
-
+env_path = os.path.join(os.path.dirname(__file__),".env")
+load_dotenv(env_path)
 llm = LLM(
     model="gemini/gemini-flash-latest",
     api_key=os.getenv("GEMINI_API_KEY")
